5a6e52b18409b7c78f36915460ff29026fcc8cf6
AkaHitsuji <yang025@e.ntu.edu.sg>
2018-03-14 11:23:16 -0700

    working on sketchShapes function. problem with approxPolyDP input

--- CVWebcam.py
+++ CVWebcam.py
@@ -16,8 +16,53 @@ def sketchPencil(image):
 def sketchShapes(image):
     img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(img_gray,127,255,1)
-    #extracting contours
-    
+    #extracting contours, we create a copy of thresh as findContours edits the image directly
+    contours, hierachy, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in contours:
+        #get approximate polygons
+        approx = cv2.approxPolyDP(c, 0.01*cv2.arcLength(c,True),True)
+
+        if len(approx) == 3:
+            shape_name = "Triangle"
+            cv2.drawContours(image,[c],0,(0,255,0),-1)
+            M = cv2.moments(c)
+            cx = int(M['m10'] / M['m00'])
+            cy = int(M['m01'] / M['m00'])
+            #-50 to bring text to center
+            cv2.putText(image, shape_name, (cx-50, cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
+        elif len(approx) == 4:
+            x,y,w,h = cv2.boundingRect(c)
+            M = cv2.moments(c)
+            cx = int(M['m10'] / M['m00'])
+            cy = int(M['m01'] / M['m00'])
+
+            # Check to see if 4-side polygon is square or rectangle
+            # cv2.boundingRect returns the top left and then width and
+            if abs(w-h) <= 3:
+                shape_name = "Square"
+
+                # Find contour center to place text at the center
+                cv2.drawContours(image, [c], 0, (0, 125 ,255), -1)
+                cv2.putText(image, shape_name, (cx-50, cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
+            else:
+                shape_name = "Rectangle"
+
+                # Find contour center to place text at the center
+                cv2.drawContours(image, [c], 0, (0, 0, 255), -1)
+                M = cv2.moments(cnt)
+                cx = int(M['m10'] / M['m00'])
+                cy = int(M['m01'] / M['m00'])
+                cv2.putText(image, shape_name, (cx-50, cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
+        elif len(approx) >= 15:
+            shape_name = "Circle"
+            cv2.drawContours(image, [c], 0, (0, 255, 255), -1)
+            M = cv2.moments(c)
+            cx = int(M['m10'] / M['m00'])
+            cy = int(M['m01'] / M['m00'])
+            cv2.putText(image, shape_name, (cx-50, cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
+
+    return image
 
 # Initialize webcam, cap is the object provided by VideoCapture
 # It contains a boolean indicating if it was sucessful (ret)
